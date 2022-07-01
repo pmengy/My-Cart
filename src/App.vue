ab468dd5f96d7ecac9d88eadcbe4ca3d8bc79a21
pmengy <936841379@qq.com>
2022-07-01 21:31:55 +0800

    完成删除选中商品和清空购物车

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,16 @@
       </tr>
       <!-- 循环渲染的元素tr -->
       <tr v-for="item in list" :key="item.id">
-        <td><input type="checkbox" v-model="item.checked" /></td>
+        <td>
+          <input type="checkbox" v-model="item.checked" />
+        </td>
         <td>{{ item.name }}</td>
         <td>{{ item.price }}</td>
-        <td><button>-</button> {{ item.count }} <button>+</button></td>
+        <td>
+          <button>-</button>
+          <input style="width: 50px" type="number" v-model="item.count" />
+          <button>+</button>
+        </td>
         <td>{{ item.price * item.count }}</td>
         <td><button @click="del(item.id)">删除</button></td>
       </tr>
@@ -24,8 +30,8 @@
       </tr>
     </table>
     <br />
-    <button>删除选中商品</button>
-    <button>清理购物车</button>
+    <button @click="delChecked">删除选中商品</button>
+    <button @click="clear">清理购物车</button>
     <br />
     <div style="margin-top: 20px">
       <h2>统计</h2>
@@ -71,11 +77,21 @@ export default {
       const index = this.list.findIndex((item) => item.id === id);
       this.list.splice(index, 1);
     },
+    // 删除选中状态的数据
+    delChecked() {
+      this.list = this.list.filter((item) => item.checked === false);
+    },
+    clear() {
+      this.list = [];
+    },
   },
+  // 全选框
   computed: {
     isAll: {
       get() {
-        return this.list.every((item) => item.checked);
+        return this.list.length === 0
+          ? false
+          : this.list.every((item) => item.checked);
       },
       set(val) {
         this.list.forEach((item) => (item.checked = val));
